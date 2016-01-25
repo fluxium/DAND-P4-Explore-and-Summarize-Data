2447d1f920e2f1a0f1296c55389175667e4837de
Mahlon Barrault <mahlonbarrault@gmail.com>
2016-01-25 09:01:39 -0700

    Adding facebook friends dataset. Adding work on facebook friends for PS3. Added code for cleaning friends dataset.

--- EDA_Course_Materials/lesson3/lesson3_problemset.R
+++ EDA_Course_Materials/lesson3/lesson3_problemset.R
@@ -1,7 +1,14 @@
+# Home Working Directory
+setwd("C:/Users/Mahlon/Source/Repos/DAND-P4-Explore-and-Summarize-Data/EDA_Course_Materials/lesson3")
+
+# Work Working Directory
+
+
 library(ggplot2)
 library('tidyr')
 library('dplyr')
 library(xlsx)
+library(lubridate)
 
 data(diamonds)
 dim(diamonds)
@@ -249,4 +256,94 @@ ggplot(aes(y = years_in_school, x = gender), data = ys) +
   geom_boxplot() +
   facet_wrap(~year)
 
+# Your task is to investigate the distribution of your friends'
+# birth months and days.
 
+# Here some questions you could answer, and we hope you think of others.
+
+# **********************************************************************
+
+# How many people share your birthday? Do you know them?
+# (Reserve time with them or save money to buy them a gift!)
+
+# Which month contains the most number of birthdays?
+
+# How many birthdays are in each month?
+
+# Which day of the year has the most number of birthdays?
+
+# Do you have at least 365 friends that have birthdays on everyday
+# of the year?
+
+# **********************************************************************
+
+# You will need to do some data munging and additional research to
+# complete this task. This task won't be easy, and you may encounter some
+# unexpected challenges along the way. We hope you learn a lot from it though.
+
+# You can expect to spend 30 min or more on this task depending if you
+# use the provided data or obtain your personal data. We also encourage you
+# to use the lubridate package for working with dates. Read over the documentation
+# in RStudio and search for examples online if you need help.
+
+# You'll need to export your Facebooks friends' birthdays to a csv file.
+# You may need to create a calendar of your Facebook friends' birthdays
+# in a program like Outlook or Gmail and then export the calendar as a
+# csv file.
+
+# Once you load the data into R Studio, you can use the strptime() function
+# to extract the birth months and birth days. We recommend looking up the
+# documentation for the function and finding examples online.
+
+# We've included some links in the Instructor Notes to help get you started.
+
+# Once you've completed your investigation, create a post in the discussions that includes:
+#       1. any questions you answered, your observations, and summary statistics
+#       2. snippets of code that created the plots
+#       3. links to the images of your plots
+
+# You can save images by using the ggsave() command.
+# ggsave() will save the last plot created.
+# For example...
+#                  qplot(x = price, data = diamonds)
+#                  ggsave('priceHistogram.png')
+
+# ggsave currently recognises the extensions eps/ps, tex (pictex),
+# pdf, jpeg, tiff, png, bmp, svg and wmf (windows only).
+
+# Copy and paste all of the code that you used for
+# your investigation below the line. Submit it when you are ready.
+# ===============================================================================
+
+bdays <- read.csv("friends_bdays.csv")
+head(bdays)
+# Sort needed because strsplit outputs an ordered list of cleaned names
+subset(bdays, select = c(Title, Start.date)) %>% arrange(Title) -> bdays
+bdays <- subset(bdays, !(Title == "")) # Excel added an empty row
+row.names(bdays) <- c(1:111) # Reindex so row.names matches output of sapply
+bdays$name <- sapply(strsplit(levels(bdays$Title), "'")[2:112], function (x) x[1] )
+bdays <- subset(bdays, select = c(name, Start.date))
+colnames(bdays) <- c("name", "birthday")
+
+# Clean the birthday column and separate in to month and day
+bdays$birthday <- strptime(bdays$birthday, "%m/%d/%Y", tz = "GMT")
+bdays$bmonth <- month(bdays$birthday)
+bdays$bday <- day(bdays$birthday)
+bdays$bmonthday <- paste(month(bdays$birthday), day(bdays$birthday))
+
+# Exploration
+ggplot(aes(x = birthday), data = bdays) +
+  geom_histogram(bins = 365) +
+  scale_x_date()
+
+# How many people share your birthday? Do you know them?
+# (Reserve time with them or save money to buy them a gift!)
+
+# Which month contains the most number of birthdays?
+
+# How many birthdays are in each month?
+
+# Which day of the year has the most number of birthdays?
+
+# Do you have at least 365 friends that have birthdays on everyday
+# of the year?
